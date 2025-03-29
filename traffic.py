784955f19aea6e90df02e39d0075e70cebd61a52
abia-blaise <abiablaise220@gmail.com>
2025-03-29 11:29:17 +0100

    initial commit

--- traffic.py
+++ traffic.py
@@ -14,7 +14,7 @@ TEST_SIZE = 0.4
 
 
 def main():
-
+    
     # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
@@ -23,7 +23,7 @@ def main():
     images, labels = load_data(sys.argv[1])
 
     # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
+    labels = tf.keras.utils.to_categorical(labels, NUM_CATEGORIES)
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
@@ -35,13 +35,18 @@ def main():
     model.fit(x_train, y_train, epochs=EPOCHS)
 
     # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
+    model.evaluate(x_test, y_test, verbose=2)
 
     # Save model to file
     if len(sys.argv) == 3:
         filename = sys.argv[2]
         model.save(filename)
         print(f"Model saved to {filename}.")
+
+    # If a model is saved, you can predict a new image
+    if len(sys.argv) == 3:
+        image_path = "path_to_new_image.jpg"  # Path to the test image
+        predict_sign(model, image_path)
 
 
 def load_data(data_dir):
@@ -58,7 +63,26 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images = []
+    labels = []
+
+    for category in range(NUM_CATEGORIES):
+        category_dir = os.path.join(data_dir, str(category))
+        if not os.path.isdir(category_dir):
+            continue
+        for img_file in os.listdir(category_dir):
+            img_path = os.path.join(category_dir, img_file)
+            img = cv2.imread(img_path)
+
+            if img is None:
+                continue
+
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            img = img / 255.0  # Normalize image pixel values to [0, 1]
+            images.append(img)
+            labels.append(category)
+
+    return images, labels
 
 
 def get_model():
@@ -67,7 +91,38 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
+                               input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+def predict_sign(model, image_path):
+    """
+    Predict the class of a traffic sign in a new image.
+    """
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+    img = img / 255.0  # Normalize image pixel values to [0, 1]
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
+
+    prediction = model.predict(img)
+    predicted_class = np.argmax(prediction, axis=1)
+    print(f"Predicted traffic sign class: {predicted_class[0]}")
 
 
 if __name__ == "__main__":
